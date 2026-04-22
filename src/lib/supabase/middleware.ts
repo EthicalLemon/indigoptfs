148c8ff08abe23e9b7b04fd03d7eab09bd4f62db
Lemon <lemuelplayzer1510@gmail.com>
2026-04-22 11:23:13 +0530

    fix: middleware type

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -13,10 +13,11 @@ export async function updateSession(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value)
-          )
+        setAll(cookiesToSet: any[]) {
+  cookiesToSet.forEach(({ name, value }) =>
+    request.cookies.set(name, value)
+  )
+}
           response = NextResponse.next({ request })
           cookiesToSet.forEach(({ name, value, options }) =>
             response.cookies.set(name, value, options)
